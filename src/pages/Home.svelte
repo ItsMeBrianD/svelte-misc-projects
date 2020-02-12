d4303ff7e0f9c300844398e12f049c401e561a0b
Brian Donald <bdonald@set-works.com>
2020-02-11 18:39:29 -0600

    Add SWLogoFancier.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,10 +2,11 @@
     import SvelteLogo from "../components/SvelteLogo.svelte";
     import Container from "../components/Container.svelte";
     import SWLogo from "../components/SWLogo.svelte";
+    import SWLogoFancier from "../components/SWLogoFancier.svelte";
 </script>
 <Container>
     <div>
-        <SWLogo/>
+        <SWLogoFancier/>
         <h1>Brian's Svelte Misc Projects</h1>
     </div>
 </Container>
